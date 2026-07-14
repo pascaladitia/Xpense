e6b72b8c76604e07bf52806a7158e5b05e0fb699
pascal <pascaladitia@gmail.com>
2026-07-14 14:39:15 +0700

    ref: remove unused movie and media-related constants in Constant.kt

--- sharedUI/src/commonMain/kotlin/com/pascal/xpense/utils/Constant.kt
+++ sharedUI/src/commonMain/kotlin/com/pascal/xpense/utils/Constant.kt
@@ -2,14 +2,6 @@ package com.pascal.xpense.utils
 
 object Constant {
     const val TIMEOUT = 300000L
-    const val ERROR_MESSAGE = "Something went wrong, check your connection!"
-    const val FORMAT_DATE = "yyyy-MM-dd"
-
-    const val POSTER_BASE_URL = "https://image.tmdb.org/t/p/"
-    const val YOUTUBE_TN_URL  = "https://img.youtube.com/vi/"
-    const val YOUTUBE_TRAILERS_URL = "https://www.youtube.com/watch?v="
-    const val W185 = "w185"
-    const val W500 = "w500"
 
     // Groq AI Chat (OpenAI-compatible). Free high-limit model.
     const val AI_BASE_URL = "https://api.groq.com/openai/v1/chat/completions"
